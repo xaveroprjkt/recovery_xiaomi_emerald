1b66c89d5c329d4137d53759a2e3a614dd4b4ae5
vannxavero <vannxaveroo@proton.me>
2025-11-10 14:00:55 +0000

    emerald: Inital Tree
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

---
+++ fox_emerald.mk
@@ -0,0 +1,13 @@
+##########################################
+##### OrangeFox specific build flags #####
+##########################################
+
+# Screen
+OF_SCREEN_H := 2400
+OF_STATUS_H := 100
+OF_STATUS_INDENT_LEFT := 48
+OF_STATUS_INDENT_RIGHT := 48
+OF_CLOCK_POS := 1
+
+# flashlight fix
+OF_FLASHLIGHT_ENABLE=1
