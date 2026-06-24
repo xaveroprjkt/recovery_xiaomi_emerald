88665cdab42bdc360ad0ffd094eb6062758b259c
vannashcroft <vannashcroft@proton.me>
2026-06-24 11:38:28 +0000

    emerald: fixup! POSTINSTALL_CONFIG
    Signed-off-by: vannashcroft <vannashcroft@proton.me>

--- device.mk
+++ device.mk
@@ -60,9 +60,15 @@ AB_OTA_PARTITIONS += \
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_PATH_system=system/bin/create_pl_dev \
+    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE) \
     POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=$(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE) \
+    POSTINSTALL_OPTIONAL_vendor=true
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 31
